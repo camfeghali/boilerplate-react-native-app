fe35ef09285eecbe1eae1fa64d6e42fd1ff2f96d
camfeghali <camfeg@gmail.com>
2022-10-23 03:57:14 +0200

    added bottom navigation icons

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {Button, StatusBar, Text, View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {HomeStackScreen} from './src/screens/home';
@@ -11,12 +10,31 @@ import {SettingsStackScreen} from './src/screens/settings';
 
 const Tab = createBottomTabNavigator();
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <StatusBar animated={true} backgroundColor="#61dafb" hidden={true} />
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home-outline';
+              } else if (route.name === 'Settings') {
+                iconName = 'settings-outline';
+              } else {
+                iconName = 'person-outline';
+              }
+
+              return <Icon name={iconName} />;
+            },
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+          })}>
           <Tab.Screen name="Home" component={HomeStackScreen} />
           <Tab.Screen name="Settings" component={SettingsStackScreen} />
           <Tab.Screen name="Profile" component={ProfileStackScreen} />
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {DetailsScreen} from './details';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HomeStack = createNativeStackNavigator();
 
@@ -34,6 +35,7 @@ function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home screen</Text>
+      <Icon name="logout" />
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
